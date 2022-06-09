d867458da53c98e90b968837452eac93eb3392c2
Warren3002 <warrenrv.30@gmail.com>
2022-06-08 18:56:22 -0600

    Tarea 02

---
+++ src/main/java/com/PracticaSemana02CSS/Sem02/Sem02Application.java
@@ -0,0 +1,13 @@
+package com.PracticaSemana02CSS.Sem02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/PracticaSemana02CSS/Sem02/Sem02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaSemana02CSS.Sem02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
